54a6560a80e932439c528305cb3ce28bcc0f3cc8
Meuse <ameuse04@gmail.com>
2021-12-02 12:48:20 -0500

    Folder filled with atom files and projects

--- atom_files/EurosToDollars/EurosToDollars.js
+++ atom_files/EurosToDollars/EurosToDollars.js
@@ -1,18 +1,18 @@
 //Example 1: actively asking user for input
 //Execute this function when the page loads...
-window.onload = function() {
+//window.onload = function() {
 //Prompt user for dollar amount
 //Read in dollar amount
-let dollars = Number(prompt("How many dollars to convert?"));
+//let dollars = Number(prompt("How many dollars to convert?"));
 //Prompt User for exchange rate
 //Read in exchange rate
-let rate = Number(prompt("What is the exchange rate?"));
+//let rate = Number(prompt("What is the exchange rate?"));
 //calculate result
-let result = dollars*rate;
+//let result = dollars*rate;
 //Present result to user
-console.log("The result is %f",result);
-}
-
+//console.log("The result is %f",result);
+//}
+/**/
 
 //Example 2: passively asking user to convert Euros
 //Execute this function when #convert button is clicked
@@ -21,13 +21,17 @@ console.log("The result is %f",result);
 let convertButton = document.getElementById("convert");
 convertButton.onclick = function() {
 //Prompt user for dollar amount (the #dollars HTML element does this)
+let dollarsElement = document.getElementById("dollars");
 //Read in dollar amount
-
+let dollars = dollarsElement.value;
 //Prompt User for exchange rate (the #rate HTML element does this)
+let rateElement = document.getElementById("rate");
 //Read in exchange rate
-
+let rate = rateElement.value;
 //calculate result
-
+let result = dollars*rate;
 //Present result to user (in HTML paragraph element)
+let resultElement = document.getElementById("result");
+resultElement.innerHTML = result;
 
 }
